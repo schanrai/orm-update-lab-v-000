7a0bfd44f22fbad7f907aea6eb7457649ed79b1b
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 15:47:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-    def update
+
+  def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
