f90bf8c50c3f2ef3501080db6ff3bcd2b5aa9916
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 16:09:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,9 +28,9 @@ class Student
   end
 
   def save
-    #if self.id
-    #self.update
-  #else
+    if self.id
+    self.update
+  else
     sql =<<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
@@ -45,11 +45,13 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
+
 
   def self.create(name, grade)
     student_instance = Student.new(name, grade)
