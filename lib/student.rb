a5f0de37c5d72268e5c3a0e0ccaf97c756ad78df
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 15:58:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,6 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    binding.pry
     Student.new(result[0], result[1], result[2])
   end
 
