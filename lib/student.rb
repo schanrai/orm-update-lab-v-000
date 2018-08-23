a6d8c3b1e1b08c73d8d18222284485158d1945e5
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 15:53:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,6 +45,12 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+
   def self.create(name, grade)
     student_instance = Student.new(name, grade)
     student_instance.save
