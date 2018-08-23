1db89dcdbcc67126abe43e33cf8bc99ab44ef452
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 15:00:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize
+  def initialize(id=nil,name,grade)
     @id = id
     @name = name
     @grade = grade
