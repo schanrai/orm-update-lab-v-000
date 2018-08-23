363f1ca997e453df2c26ba069895a8df44e50385
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 15:08:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,6 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
   def initialize(id=nil,name,grade)
     @id = id
     @name = name
